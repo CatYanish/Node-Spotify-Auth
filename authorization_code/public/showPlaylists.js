475bd40e644b75e9b237900e50669456f464f2ab
Cat Yanish <catyanish@gmail.com>
2019-06-19 15:38:54 -0500

    add react and babel

---
+++ authorization_code/public/showPlaylists.js
@@ -0,0 +1,23 @@
+'use strict';
+
+class ShowPlaylists extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    if (this.state.liked) {
+      return 'You liked this.';
+    }
+
+    return (
+      <button onClick={() => this.setState({ liked: true }) }>
+        Like
+      </button>
+    );
+  }
+}
+
+let domContainer = document.querySelector('#show-playlists');
+ReactDOM.render(<ShowPlaylists />, domContainer);
