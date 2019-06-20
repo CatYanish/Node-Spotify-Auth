35d42059c4559b3ddeb8c24507259b949b7efbf5
Cat Yanish <catyanish@gmail.com>
2019-06-19 19:02:50 -0500

    access token is now accessble in react component

--- authorization_code/public/showPlaylists.js
+++ authorization_code/public/showPlaylists.js
@@ -3,19 +3,93 @@
 class ShowPlaylists extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { liked: false };
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: []
+    };
+  }
+
+  componentDidMount() {
+
+    function getHashParams() {
+      var hashParams = {};
+      var e, r = /([^&;=]+)=?([^&;]*)/g,
+          q = window.location.hash.substring(1);
+      while ( e = r.exec(q)) {
+         hashParams[e[1]] = decodeURIComponent(e[2]);
+      }
+      return hashParams;
+    }
+
+    var userProfileSource = document.getElementById('user-profile-template').innerHTML,
+        userProfileTemplate = Handlebars.compile(userProfileSource),
+        userProfilePlaceholder = document.getElementById('user-profile');
+
+    var oauthSource = document.getElementById('oauth-template').innerHTML,
+        oauthTemplate = Handlebars.compile(oauthSource),
+        oauthPlaceholder = document.getElementById('oauth');
+
+    var params = getHashParams();
+
+    var access_token = params.access_token,
+        refresh_token = params.refresh_token,
+        error = params.error;
+
+        console.log(access_token);
+
+
+    // fetch("https://api.example.com/items", { headers: {
+    //         'Authorization': 'Bearer ' + access_token
+    //       }})
+    //
+    // // document.getElementById('get-my-playlists').addEventListener('click', function() {
+    // //   $.ajax({
+    // //       url: 'https://api.spotify.com/v1/me/playlists',
+    // //
+    // //       success: function(response) {
+    // //         console.log(response);
+    // //       }
+    // //   })
+    // // }, false);
+    //
+    //   .then(res => res.json())
+    //   .then(
+    //     (result) => {
+    //       this.setState({
+    //         isLoaded: true,
+    //         items: result.items
+    //       });
+    //     },
+    //     // Note: it's important to handle errors here
+    //     // instead of a catch() block so that we don't swallow
+    //     // exceptions from actual bugs in components.
+    //     (error) => {
+    //       this.setState({
+    //         isLoaded: true,
+    //         error
+    //       });
+    //     }
+    //   )
   }
 
   render() {
-    if (this.state.liked) {
-      return 'You liked this.';
+    const { error, isLoaded, items } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <ul>
+          {items.map(item => (
+            <li key={item.name}>
+              {item.name} {item.price}
+            </li>
+          ))}
+        </ul>
+      );
     }
-
-    return (
-      <button onClick={() => this.setState({ liked: true }) }>
-        Like
-      </button>
-    );
   }
 }
 
