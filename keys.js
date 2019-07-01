5241dd5656b2df8bec032d8704ab88437f36e581
Catherine Yanish <catyanish@gmail.com>
2019-07-01 17:04:56 -0500

    Delete keys.js

--- keys.js
+++
@@ -1,3 +0,0 @@
-exports.CLIENT_ID = '434d35e92a7b4464a9629918c0074ce2'; // Your client id
-exports.CLIENT_SECRET = '7f1630c4c4b74833ac5ecf9a07416c2e'; // Your secret
-exports.REDIRECT_URI = 'http://localhost:8888/callback'; // Your redirect uri
